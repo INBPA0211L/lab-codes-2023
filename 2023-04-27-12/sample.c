2747d226e56749e3351d361d6312ad65f57ae71f
Tóth Róbert <tothr94@gmail.com>
2023-04-27 20:25:01 +0200

    Add labs of 04-27

---
+++ 2023-04-27-12/sample.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char name[31];
+    char city[31];
+    int runways;
+    int time;
+} AIRPORT;
+
+int cmp(const void *a, const void *b) {
+    AIRPORT *left = (AIRPORT *) a;
+    AIRPORT *right = (AIRPORT *) b;
+
+    if(left->runways != right->runways) {
+        return - (left->runways 
+                    - right->runways);
+    }
+
+    if(left->time != right->time) {
+        return - (left->time 
+                    - right->time);
+    }
+
+    return strcmp(left->name, right->name);
+}
+
+int query(AIRPORT *airports, int length) {
+    int max = airports[0].runways;
+    for(int i=1; i<length;i++) {
+        if(airports[i].runways > max) {
+            max = airports[i].runways;
+        }
+    }
+    return max;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        fprintf(stderr, "No 1st argument\n");
+        return 1;
+    }
+
+    FILE *fin = fopen(argv[1], "r");
+    if(fin == NULL) { // !fin
+        fprintf(stderr,
+             "File doesn't exist\n");
+        return 2;
+    }
+
+    char line[102];
+    int length = 0;
+    AIRPORT airports[20];
+
+    while(fgets(line, 102, fin)) {
+        line[strlen(line) - 1] = '\0';
+
+        strcpy(airports[length].name ,
+                  strtok(line, ";"));
+        strcpy(airports[length].city,       
+            strtok(NULL, ";"));
+        airports[length].runways =
+            atoi(strtok(NULL, ";"));
+        airports[length].time =
+            atoi(strtok(NULL, ";"));
+        length++;
+    }
+
+    fclose(fin);
+
+    if(argc < 3) {
+        fprintf(stderr, "No 2nd argument\n");
+        return 3;
+    }
+
+    FILE *fout = fopen(argv[2], "w");
+    if(!fout) {
+        fprintf(stderr, "Cannot open 2nd file\n");
+        return 4;
+    }
+
+    qsort(airports, length, 
+        sizeof(AIRPORT), cmp);
+
+    for(int i=0; i<length; i++) {
+        fprintf(fout, "%s;%s;%d;%d\n",
+            airports[i].name,
+            airports[i].city,
+            airports[i].runways,
+            airports[i].time);
+    }
+
+    fclose(fout);
+
+    return EXIT_SUCCESS;
+}
